24d612d1eb0a6ecdbcf9a6dfbdad9031ff551708
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-03 23:28:20 -0400

    new page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>AI Stock Price Predictions</h1>
-
+     
+    <h1 className="text-5xl font-sans bg-gradient-to-r from-green-300 to-green-700 p-5 rounded">AI Stocks Predictions</h1>
       <div>
         <Link href='/advisor'>Get started</Link>
       </div>
