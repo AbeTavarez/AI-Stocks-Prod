c9f1513d7e6556efd3475e66eee87d2f2d08c94b
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-04 23:03:41 -0400

    header component

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import { PropsWithChildren } from "react";
+import Navbar from "./navbar";
 
 export default function Header({ children }: PropsWithChildren) {
   return (
     <header className="flex items-center justify-around p-5">
-         <h1 className="text-3xl font-sans">AI Stocks 🚀</h1>
-        {children}</header>
+      <Link href="/">
+        <h1 className="text-3xl font-sans">AI Stocks 🚀</h1>
+      </Link>
+     <Navbar />
+    </header>
   );
 }
