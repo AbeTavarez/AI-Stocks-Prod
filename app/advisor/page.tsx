a81ee8cc00d7138164b684f6454302aafa943054
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-05 23:06:45 -0400

    page layout updated

--- app/advisor/page.tsx
+++ app/advisor/page.tsx
@@ -1,19 +1,14 @@
-import StockTickerPrice from "@/components/stock-picker-form";
+import StockPickerForm from "@/components/stock-picker-form";
 
 export default function Advisor() {
   return (
-    <div className="h-screen p-20">
-      <h1 className="text-2xl mb-10">AI Stock Price Agent</h1>
+    <div className="h-screen p-20 font-sans">
+      <h1 className="text-3xl mb-10">AI Stock Price Advisor</h1>
 
+      <StockPickerForm />
 
-      <div>
-        {/* IMAGE  */}
-        <StockTickerPrice />
-      </div>
-
-
-      <div className="text-3xl bg-red-600">
-        This is NOT real financial advice!
+      <div className="text-3xl bg-red-600 p-2">
+        Disclaimer: This is NOT real financial advice!
       </div>
     </div>
   );
