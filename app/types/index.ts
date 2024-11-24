4c66c8da7fc664e7098ba4e9d7b75c5df1888bc7
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-11-23 22:48:39 -0500

    sentiment types

--- app/types/index.ts
+++ app/types/index.ts
@@ -5,3 +5,27 @@ export type Prediction = {
   nextDayPrediction: number;
   analysis: string;
 };
+
+export type RedditPost = {
+  data: {
+    title: string;
+    selftext: string;
+  };
+};
+
+export type ProcessedPost = {
+  title: string;
+  content: string;
+};
+
+export type SentimentCount = {
+  positive: number;
+  negative: number;
+  neutral: number;
+};
+
+export type SentimentResult = {
+  mainSentiment: string | undefined,
+  posts: ProcessedPost[],
+  sentimentCount: SentimentCount 
+}
