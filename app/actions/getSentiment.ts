5f94d1c4b6d5556eb0cc7d9ad46b775ef7a24fec
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-12-08 19:49:44 -0500

    change on reddit api request d1 app

--- app/actions/getSentiment.ts
+++ app/actions/getSentiment.ts
@@ -19,7 +19,7 @@ export async function getSentiment(ticker: string) {
           "Content-Type": "application/x-www-form-urlencoded",
           Authorization: `Basic ${auth}`,
         },
-        body: `grant_type=client_credentials`,
+        body: `grant_type=client_credentials&redirect_uri=${process.env.REDIRECT_URI}`,
       },
     );
 
